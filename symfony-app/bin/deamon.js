e2014ff2702667966ee0d8175039bdd0d1c1de8d
Jan Thomas <Jan Thomas>
2015-03-27 20:33:51 +0100

    Added a node js deamon stub for the measurement command

---
+++ symfony-app/bin/deamon.js
@@ -0,0 +1,104 @@
+var http = require('http');
+var path = require('path');
+var url = require('url');
+var qs = require('querystring');
+var util = require('util');
+var cp = require('child_process');
+var fs = require('fs');
+
+// @see http://labs.telasocial.com/nodejs-forever-daemon/
+// forever -o app/logs/nodejs-deamon.log -e app/logs/nodejs-deamon.log bin/deamon.js
+
+/* Hashmap for running sub processes */
+var processes = {};
+
+/* Create server */
+var server = http.createServer(function (req, resp) {
+    var parsedUrl = url.parse(req.url);
+    var params = qs.parse(parsedUrl.query);
+
+    if ('/spawn' === parsedUrl.pathname && params.measurementId) {
+        var php = spawn(params.measurementId);
+
+        jsonResponse(resp, 200, {
+            pid: php.pid,
+            msg: 'process with pid ' + php.pid + ' started for measurement '
+        });
+    } else if ('/kill' === parsedUrl.pathname && params.measurementId) {
+        var res = kill(params.measurementId);
+
+        jsonResponse(resp, res ? 200 : 404, {
+            status: res,
+            msg: 'killing process for measurement ' + params.measurementId
+        });
+    } else {
+        jsonResponse(resp, 404, {
+            msg: 'Not found. Try /spawn?measurementId=123 or /kill?measurementId=123'
+        });
+    }
+});
+server.listen(1025);
+console.log('wlanthermo deamon server now listens on port 1025');
+
+/* Span process */
+function spawn(measurementId) {
+    if (processes[measurementId]) {
+        kill(measurementId);
+    }
+
+    var php  = cp.spawn('php', [__dirname + '/../app/console', 'app:measurement', measurementId]);
+    php.stdout.on('data', function (data) {
+        util.log('[OUT pid ' + php.pid + ', measurementId ' + measurementId + ']: ' + data);
+    });
+    php.stderr.on('data', function (data) {
+        util.log('[ERR pid ' + php.pid + ', measurementId ' + measurementId + ']: ' + data);
+    });
+    php.on('exit', function (code) {
+        util.log('[EXIT pid ' + php.pid + ', measurementId ' + measurementId + ']: code ' + code);
+    });
+
+    setTimeout(function() {
+        processes[measurementId] = php;
+    }, 0);
+
+    return php;
+};
+
+/* Kill process */
+var kill = function(measurementId) {
+    var php = processes[measurementId];
+    if (php) {
+        php.kill('SIGINT');
+        delete processes[measurementId];
+        return true;
+    } else {
+        return false;
+    }
+};
+
+/* Output a JSON response */
+var jsonResponse = function(resp, status, obj) {
+    resp.writeHead(status, {
+        'Content-Type': 'application/json; charset=utf-8'
+    });
+    obj.status = status;
+    resp.write(JSON.stringify(obj));
+    resp.end();
+};
+
+/* Clean shutdown */
+var onExit = function () {
+    processes && util.log('wlanthermo deamon server shuts down');
+
+    for (var key in processes) {
+        if (processes.hasOwnProperty(key)) {
+            processes[key].kill('SIGINT');
+        }
+    }
+
+    processes = null;
+
+    process.exit(0);
+};
+process.on('SIGINT', onExit);
+process.on('exit', onExit);
---
+++ symfony-app/src/AppBundle/Command/MeasurementCommand.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace AppBundle\Command;
+
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class MeasurementCommand extends ContainerAwareCommand
+{
+    protected function configure()
+    {
+        $this
+            ->setName('app:measurement')
+            ->setDescription('Wlanthermo measurement command')
+            ->addArgument(
+                'measurementId',
+                InputArgument::REQUIRED,
+                'Please provide a measurement id'
+            );
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $id = (int)$input->getArgument('measurementId');
+
+        for ($i = 0; true; $i++) {
+            $output->writeln($id . ', iteration: ' . $i . ', memory usage:' . (memory_get_usage()/1024/1024));
+            sleep(1);
+        }
+    }
+} 
